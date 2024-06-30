db2ec0300da60fef9bd90a2c13fdfff80ab7cf3e
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-06-30 18:55:55 +0700

    fix detail sponsor image

--- resources/views/event/detail.blade.php
+++ resources/views/event/detail.blade.php
@@ -1,67 +1,72 @@
 @extends('layouts.event_layout')
 @section('content')
-    <div class=" flex flex-col m-3">
-        <div class="flex justify-between items-center mx-9">
-            <div>
-                <div class="flex flex-col justify-start gap-3 md:flex-row md:items-center">
-                    <p class="font-semibold text-[35px]">{{$sponsor->name}}</p>
-                    <div class="badge badge-neutral truncate  ... h-7">{{$sponsor->category->category}}</div>
+<div class=" flex flex-col m-3">
+    <div class="flex justify-between items-center mx-9">
+        <div>
+            <div class="flex flex-col justify-start gap-3 md:flex-row md:items-center">
+                <p class="font-semibold text-[35px]">{{$sponsor->name}}</p>
+                <div class="badge badge-neutral truncate  ... h-7">{{$sponsor->category->category}}</div>
+            </div>
+            <div class="flex flex-row gap-3 font-medium text-[18px]">
+                <div class="flex items-center gap-2">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <p>{{$sponsor->address}}</p>
                 </div>
-                <div class="flex flex-row gap-3 font-medium text-[18px]">
-                    <div class="flex items-center gap-2">
-                        <i class="fa-solid fa-location-dot"></i>
-                        <p>{{$sponsor->address}}</p>
-                    </div>
-                    <div class="flex items-center gap-2">
-                        <i class="fa-regular fa-calendar-days"></i>
-                        <p>{{$sponsor->max_submission_date}}</p>
-                    </div>
+                <div class="flex items-center gap-2">
+                    <i class="fa-regular fa-calendar-days"></i>
+                    <p>{{$sponsor->max_submission_date}}</p>
                 </div>
             </div>
-            <label for="" class="btn btn-neutral">
-                <div class="flex items-center gap-2">
-                    <i class="fa-solid fa-share-nodes"></i>
-                    <h1>bagikan</h1>
-                </div>
-            </label>
         </div>
-        <div class="flex justify-center">
-            <figure class="w-screen ">
+        <label for="" class="btn btn-neutral">
+            <div class="flex items-center gap-2">
+                <i class="fa-solid fa-share-nodes"></i>
+                <h1>bagikan</h1>
+            </div>
+        </label>
+    </div>
+    <div class="flex justify-center">
+        <!-- <figure class="w-screen ">
                 <img class="h-[400px] object-cover w-[93%] rounded-md mx-10 my-6" src="http://127.0.0.1:8080/{{$sponsor->image}}"
                     alt="">
-            </figure>
-        </div>
-        <div class="mx-9">
-            <p class="font-bold text-[25px]">Deskripsi</p>
-            <p>
-                {{$sponsor->description}}
-            </p>
-        </div>
-
-        <button class="btn btn-neutral mt-4 mx-9" onclick="my_modal_3.showModal()">Kirim proposal</button>
-        <dialog id="my_modal_3" class="modal">
-            <div class="modal-box">
-                <form method="dialog">
-                    <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                </form>
-                <label class="form-control w-full max-w-xs">
-                    <div class="label">
-                        <span class="label-text">Pilih acara terbaikmu !</span>
-
-                    </div>
-                    <form action="/event/sponsor/detail" method="POST">
-                        @csrf
-                        <select name="id_event" class="select select-bordered">
-                            <option disabled selected>Pilih salah satu</option>
-                            @foreach ($events as $event)
-                                <option value={{$event->id}}>{{ $event->name }}</option>
-                            @endforeach
-                        </select>
-                </label>
-                <input type="hidden" name="id_sponsor" value={{$sponsor->id}}>
-                <button class="btn btn-active btn-neutral mt-4">Kirim</button>
-                </form>
+            </figure> -->
+        <div class="avatar">
+            <div class="h-[400px] my-6 rounded">
+                <img src="http://127.0.0.1:8080/{{$sponsor->image}}" />
             </div>
-        </dialog>
+        </div>
     </div>
+    <div class="mx-9">
+        <p class="font-bold text-[25px]">Deskripsi</p>
+        <p>
+            {{$sponsor->description}}
+        </p>
+    </div>
+
+    <button class="btn btn-neutral mt-4 mx-9" onclick="my_modal_3.showModal()">Kirim proposal</button>
+    <dialog id="my_modal_3" class="modal">
+        <div class="modal-box">
+            <form method="dialog">
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            </form>
+            <label class="form-control w-full max-w-xs">
+                <div class="label">
+                    <span class="label-text">Pilih acara terbaikmu !</span>
+
+                </div>
+                <form action="/event/sponsor/detail" method="POST">
+                    @csrf
+                    <select name="id_event" class="select select-bordered">
+                        <option disabled selected>Pilih salah satu</option>
+                        @foreach ($events as $event)
+                        <option value={{$event->id}}>{{ $event->name }}</option>
+                        @endforeach
+                    </select>
+            </label>
+            <input type="hidden" name="id_sponsor" value={{$sponsor->id}}>
+            <button class="btn btn-active btn-neutral mt-4">Kirim</button>
+            </form>
+        </div>
+    </dialog>
+</div>
 @endsection
--- resources/views/sponsor/detailEvent.blade.php
+++ resources/views/sponsor/detailEvent.blade.php
@@ -1,77 +1,74 @@
 @extends('layouts.sponsor_layout')
 @section('content')
-    <div class="m-11">
-        <div class="mx-40 flex justify-center gap-14">
-            <div class="avatar">
-                <div class="w-[400px] rounded-xl">
-                    <img class=" rounded-xl drop-shadow-2xl"
-                    src="http://localhost:8080/{{$transaction->event->image}}" alt="">
+<div class="m-11">
+    <div class="mx-40 flex justify-center gap-14">
+        <div class="avatar">
+            <div class="w-[400px] rounded-xl">
+                <img class=" rounded-xl drop-shadow-2xl" src="http://localhost:8080/{{$transaction->event->image}}" alt="">
+            </div>
+        </div>
+        <!-- <div class="h-[552px] flex justify-center items-center">
+            <img class=" rounded-xl drop-shadow-2xl" src="http://localhost:8080/{{$transaction->event->image}}" alt="">
+        </div> -->
+        <div>
+            <h1 class="font-semibold text-[50px]">{{ $transaction->event->name }}</h1>
+            <div class="flex items-center gap-5">
+                <div class="px-2 py-2 rounded-xl">
+                    <i class="fa-regular text-4xl fa-calendar"></i>
                 </div>
-              </div>
-            {{-- <div class="h-[552px] flex justify-center items-center">
-                <img class=" rounded-xl drop-shadow-2xl"
-                    src="http://localhost:8080/{{$transaction->event->image}}" alt="">
-            </div> --}}
-            <div>
-                <h1 class="font-semibold text-[50px]">{{ $transaction->event->name }}</h1>
-                <div class="flex items-center gap-5">
-                    <div class="px-2 py-2 rounded-xl">
-                        <i class="fa-regular text-4xl fa-calendar"></i>
-                    </div>
-                    <div class="">
-                        <h1 class="font-semibold text-[20px]">
-                            {{ date('d/m/Y', strtotime($transaction->event->start_date)) }} -
-                            {{ date('d/m/Y', strtotime($transaction->event->end_date)) }}</h1>
-                            <h1 class="text-[#8f8f8f]">Tanggal mulai - Tanggal berakhir</h1>
-                    </div>
-                </div>
-                <div class="flex items-center gap-6 mt-3">
-                    <div class="px-2 py-2 rounded-xl">
-                        <i class="fa-solid text-4xl fa-location-dot"></i>
-                    </div>
-                    <div class="">
-                        <h1 class="font-semibold text-[20px]">{{ $transaction->event->location }}</h1>
-                        <h1 class="text-[#8f8f8f]">alamat acara</h1>
-                    </div>
-                </div>
-                <div class="flex items-center gap-5 mt-3">
-                    <div class="px-2 py-2 rounded-xl">
-                        <i class="fa-regular text-4xl fa-user"></i>
-                    </div>
-                    <div class="">
-                        <h1 class="font-semibold text-[20px]">{{ $event->user->name }}</h1>
-                        <h1 class="text-[#8f8f8f]">Penanggung jawab</h1>
-                    </div>
-                </div>
-                <div class="flex justify-start mt-5">
-                    <a href="http://localhost:8080/{{ $transaction->event->proposal }}"
-                        class="flex gap-2 bg-neutral text-white px-40 py-3 rounded-xl">
-                        <i class="fa-solid fa-download"></i>
-                        <h1 class="font-semibold">Review proposal</h1>
-                    </a>
+                <div class="">
+                    <h1 class="font-semibold text-[20px]">
+                        {{ date('d/m/Y', strtotime($transaction->event->start_date)) }} -
+                        {{ date('d/m/Y', strtotime($transaction->event->end_date)) }}
+                    </h1>
+                    <h1 class="text-[#8f8f8f]">Tanggal mulai - Tanggal berakhir</h1>
                 </div>
             </div>
+            <div class="flex items-center gap-6 mt-3">
+                <div class="px-2 py-2 rounded-xl">
+                    <i class="fa-solid text-4xl fa-location-dot"></i>
+                </div>
+                <div class="">
+                    <h1 class="font-semibold text-[20px]">{{ $transaction->event->location }}</h1>
+                    <h1 class="text-[#8f8f8f]">alamat acara</h1>
+                </div>
+            </div>
+            <div class="flex items-center gap-5 mt-3">
+                <div class="px-2 py-2 rounded-xl">
+                    <i class="fa-regular text-4xl fa-user"></i>
+                </div>
+                <div class="">
+                    <h1 class="font-semibold text-[20px]">{{ $event->user->name }}</h1>
+                    <h1 class="text-[#8f8f8f]">Penanggung jawab</h1>
+                </div>
+            </div>
+            <div class="flex justify-start mt-5">
+                <a href="http://localhost:8080/{{ $transaction->event->proposal }}" class="flex gap-2 bg-neutral text-white px-40 py-3 rounded-xl">
+                    <i class="fa-solid fa-download"></i>
+                    <h1 class="font-semibold">Review proposal</h1>
+                </a>
+            </div>
+        </div>
 
 
-        </div>
-        <div class="mx-40 my-5 flex flex-col items-start">
-            <h1 class="font-semibold text-[21px]">Tentang Acara</h1>
-            <p class="text-justify">{{ $transaction->event->description }}</p>
-        </div>
-        <div class="flex justify-center gap-5 ">
-            <div>
-                <button class="px-52 py-3 bg-green-500 font-semibold text-white rounded-2xl"
-                    onclick="my_modal_terima.showModal()">Terima</button>
-                <dialog id="my_modal_terima" class="modal">
-                    <div class="modal-box">
-                        <form method="dialog">
-                            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                            <h3 class="font-bold text-lg mt-5 mb-4">Kirim pesan untuk Event organizer</h3>
-                        </form>
-                        <div>
-                             <form action="/sponsor/review" method="post">
-                             @csrf
-                        @method('patch')
+    </div>
+    <div class="mx-40 my-5 flex flex-col items-start">
+        <h1 class="font-semibold text-[21px]">Tentang Acara</h1>
+        <p class="text-justify">{{ $transaction->event->description }}</p>
+    </div>
+    <div class="flex justify-center gap-5 ">
+        <div>
+            <button class="px-52 py-3 bg-green-500 font-semibold text-white rounded-2xl" onclick="my_modal_terima.showModal()">Terima</button>
+            <dialog id="my_modal_terima" class="modal">
+                <div class="modal-box">
+                    <form method="dialog">
+                        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                        <h3 class="font-bold text-lg mt-5 mb-4">Kirim pesan untuk Event organizer</h3>
+                    </form>
+                    <div>
+                        <form action="/sponsor/review" method="post">
+                            @csrf
+                            @method('patch')
                             <label class="input input-bordered flex items-center gap-2">
                                 <input type="number" name="total_fund" class="grow" placeholder="Jumlah Dana Sponsor Yang Akan Diberikan" />
                             </label>
@@ -82,36 +79,35 @@
                             <input type="hidden" name="id" value={{$transaction->id}}>
                             <button class="px-10 py-2 rounded-2xl text-white bg-neutral mt-5 font-semibold">kirim</button>
                         </form>
-                        </div>
                     </div>
-                </dialog>
+                </div>
+            </dialog>
 
 
-            </div>
-            <div>
-                <button class="px-52 py-3 bg-red-600 font-semibold text-white rounded-2xl"
-                    onclick="my_modal_tolak.showModal()">Tolak</button>
-                <dialog id="my_modal_tolak" class="modal">
-                    <div class="modal-box">
-                        <form method="dialog">
-                            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                            <h3 class="font-bold text-lg mt-5 mb-4">Kirim alasan untuk Event organizer</h3>
-                        </form>
-                        <div>
-                            <form action="/sponsor/review" method="post">
+        </div>
+        <div>
+            <button class="px-52 py-3 bg-red-600 font-semibold text-white rounded-2xl" onclick="my_modal_tolak.showModal()">Tolak</button>
+            <dialog id="my_modal_tolak" class="modal">
+                <div class="modal-box">
+                    <form method="dialog">
+                        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                        <h3 class="font-bold text-lg mt-5 mb-4">Kirim alasan untuk Event organizer</h3>
+                    </form>
+                    <div>
+                        <form action="/sponsor/review" method="post">
                             @csrf
-                        @method('patch')
+                            @method('patch')
                             <label class="input input-bordered flex items-center gap-2">
                                 <input type="text" name="comment" class="grow" placeholder="komentar" />
                             </label>
                             <input type="hidden" name="id_status" value="3">
-                             <input type="hidden" name="id" value={{$transaction->id}}>
+                            <input type="hidden" name="id" value={{$transaction->id}}>
                             <button class="px-10 py-2 rounded-2xl text-white bg-neutral mt-5 font-semibold">kirim</button>
                         </form>
-                        </div>
                     </div>
-                </dialog>
-            </div>
+                </div>
+            </dialog>
         </div>
     </div>
+</div>
 @endsection
